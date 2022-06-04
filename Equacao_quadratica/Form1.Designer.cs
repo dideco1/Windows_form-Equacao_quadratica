13bc0abb5477aa210f34b1d33020ecc9039f8411
dideco1 <diegobaro.si@outlook.com>
2022-06-04 12:24:20 -0300

    Alterando as cores

--- Equacao_quadratica/Form1.Designer.cs
+++ Equacao_quadratica/Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txt_valor_a = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txt_valor_b = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
             this.txt_valor_c = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.btn_calcular = new System.Windows.Forms.Button();
+            this.txt_valor_b = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_valor_a = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.lbl_delta = new System.Windows.Forms.Label();
             this.lbl_x1 = new System.Windows.Forms.Label();
@@ -56,6 +56,7 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.txt_valor_a);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(64)))), ((int)(((byte)(51)))));
             this.groupBox1.Location = new System.Drawing.Point(12, 0);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(369, 265);
@@ -63,45 +64,23 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Valores";
             // 
-            // txt_valor_a
+            // btn_calcular
             // 
-            this.txt_valor_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_valor_a.Location = new System.Drawing.Point(158, 27);
-            this.txt_valor_a.Name = "txt_valor_a";
-            this.txt_valor_a.Size = new System.Drawing.Size(164, 29);
-            this.txt_valor_a.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Valor de A (x²):";
-            // 
-            // txt_valor_b
-            // 
-            this.txt_valor_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_valor_b.Location = new System.Drawing.Point(158, 70);
-            this.txt_valor_b.Name = "txt_valor_b";
-            this.txt_valor_b.Size = new System.Drawing.Size(164, 29);
-            this.txt_valor_b.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(11, 70);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 24);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Valor de B (x):";
+            this.btn_calcular.BackColor = System.Drawing.Color.Black;
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_calcular.Location = new System.Drawing.Point(113, 204);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(140, 55);
+            this.btn_calcular.TabIndex = 6;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = false;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
             // 
             // txt_valor_c
             // 
             this.txt_valor_c.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_valor_c.ForeColor = System.Drawing.Color.CornflowerBlue;
             this.txt_valor_c.Location = new System.Drawing.Point(158, 115);
             this.txt_valor_c.Name = "txt_valor_c";
             this.txt_valor_c.Size = new System.Drawing.Size(164, 29);
@@ -111,27 +90,58 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(64)))), ((int)(((byte)(51)))));
             this.label3.Location = new System.Drawing.Point(11, 115);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 24);
             this.label3.TabIndex = 4;
             this.label3.Text = "Valor de C:";
             // 
-            // btn_calcular
+            // txt_valor_b
             // 
-            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_calcular.Location = new System.Drawing.Point(113, 204);
-            this.btn_calcular.Name = "btn_calcular";
-            this.btn_calcular.Size = new System.Drawing.Size(140, 55);
-            this.btn_calcular.TabIndex = 6;
-            this.btn_calcular.Text = "Calcular";
-            this.btn_calcular.UseVisualStyleBackColor = true;
-            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            this.txt_valor_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_valor_b.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.txt_valor_b.Location = new System.Drawing.Point(158, 70);
+            this.txt_valor_b.Name = "txt_valor_b";
+            this.txt_valor_b.Size = new System.Drawing.Size(164, 29);
+            this.txt_valor_b.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(64)))), ((int)(((byte)(51)))));
+            this.label2.Location = new System.Drawing.Point(11, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor de B (x):";
+            // 
+            // txt_valor_a
+            // 
+            this.txt_valor_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_valor_a.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.txt_valor_a.Location = new System.Drawing.Point(158, 27);
+            this.txt_valor_a.Name = "txt_valor_a";
+            this.txt_valor_a.Size = new System.Drawing.Size(164, 29);
+            this.txt_valor_a.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(64)))), ((int)(((byte)(51)))));
+            this.label1.Location = new System.Drawing.Point(11, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor de A (x²):";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(64)))), ((int)(((byte)(51)))));
             this.label4.Location = new System.Drawing.Point(23, 297);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(57, 24);
@@ -142,24 +152,29 @@
             // 
             this.lbl_delta.AutoSize = true;
             this.lbl_delta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_delta.ForeColor = System.Drawing.Color.PaleTurquoise;
             this.lbl_delta.Location = new System.Drawing.Point(86, 297);
             this.lbl_delta.Name = "lbl_delta";
-            this.lbl_delta.Size = new System.Drawing.Size(0, 24);
+            this.lbl_delta.Size = new System.Drawing.Size(23, 24);
             this.lbl_delta.TabIndex = 8;
+            this.lbl_delta.Text = "A";
             // 
             // lbl_x1
             // 
             this.lbl_x1.AutoSize = true;
             this.lbl_x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_x1.ForeColor = System.Drawing.Color.PaleTurquoise;
             this.lbl_x1.Location = new System.Drawing.Point(295, 297);
             this.lbl_x1.Name = "lbl_x1";
-            this.lbl_x1.Size = new System.Drawing.Size(0, 24);
+            this.lbl_x1.Size = new System.Drawing.Size(23, 24);
             this.lbl_x1.TabIndex = 10;
+            this.lbl_x1.Text = "A";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(64)))), ((int)(((byte)(51)))));
             this.label7.Location = new System.Drawing.Point(232, 297);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(33, 24);
@@ -170,15 +185,18 @@
             // 
             this.lbl_x2.AutoSize = true;
             this.lbl_x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_x2.ForeColor = System.Drawing.Color.PaleTurquoise;
             this.lbl_x2.Location = new System.Drawing.Point(295, 339);
             this.lbl_x2.Name = "lbl_x2";
-            this.lbl_x2.Size = new System.Drawing.Size(0, 24);
+            this.lbl_x2.Size = new System.Drawing.Size(23, 24);
             this.lbl_x2.TabIndex = 12;
+            this.lbl_x2.Text = "A";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(64)))), ((int)(((byte)(51)))));
             this.label8.Location = new System.Drawing.Point(232, 339);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(37, 24);
@@ -187,30 +205,35 @@
             // 
             // btn_limpar
             // 
+            this.btn_limpar.BackColor = System.Drawing.Color.Black;
             this.btn_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpar.ForeColor = System.Drawing.Color.PaleTurquoise;
             this.btn_limpar.Location = new System.Drawing.Point(12, 383);
             this.btn_limpar.Name = "btn_limpar";
             this.btn_limpar.Size = new System.Drawing.Size(140, 55);
             this.btn_limpar.TabIndex = 7;
             this.btn_limpar.Text = "Novo Cálculo";
-            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.UseVisualStyleBackColor = false;
             this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
             // 
             // btn_sair
             // 
+            this.btn_sair.BackColor = System.Drawing.Color.Black;
             this.btn_sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sair.ForeColor = System.Drawing.Color.PaleTurquoise;
             this.btn_sair.Location = new System.Drawing.Point(241, 383);
             this.btn_sair.Name = "btn_sair";
             this.btn_sair.Size = new System.Drawing.Size(140, 55);
             this.btn_sair.TabIndex = 13;
             this.btn_sair.Text = "Sair";
-            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.UseVisualStyleBackColor = false;
             this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(393, 450);
             this.Controls.Add(this.btn_sair);
             this.Controls.Add(this.btn_limpar);
@@ -221,6 +244,7 @@
             this.Controls.Add(this.lbl_delta);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.Cyan;
             this.Name = "Form1";
             this.Text = "Equação Quadrática";
             this.groupBox1.ResumeLayout(false);
